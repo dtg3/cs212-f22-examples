108ba6bc6fc234fff272c10ab5f0485f825c159f
Drew Guarnera <dguarnera@wooster.edu>
2022-09-14 12:15:10 -0400

    Add example of working pointer pass.

--- class-examples/memory/stack-only.c
+++ class-examples/memory/stack-only.c
@@ -13,7 +13,7 @@ void repeat(int count) {
     }
 }
 
-char* repeat_letter(int count, char letter) {
+char* repeat_letter_broken(int count, char letter) {
     char string[count + 1];
     for (int i = 0; i < count; ++i) {
         string[i] = letter;
@@ -23,20 +23,42 @@ char* repeat_letter(int count, char letter) {
     // This doesn't work because string is destroyed
     //  once the function returns. Verify this with
     //  the visualization link at the top of the file.
-    printf("location: %p\n", string);
+    printf("%s location: %p\n", __FUNCTION__, string);
     return string;
+}
+
+void repeat_letter_working(char *array, int count, char letter) {
+    
+    for (int i = 0; i < count; ++i) {
+        array[i] = letter;
+    }
+    array[count] = '\0';
+
+    // This doesn't work because string is destroyed
+    //  once the function returns. Verify this with
+    //  the visualization link at the top of the file.
+    printf("%s location: %p\n", __FUNCTION__, array);
 }
 
 int main(int argc, char *argv[]) {
     int my_count = 5;
     repeat(my_count);
 
-    char * repeat_string = repeat_letter(my_count, 'a');
-    printf("location: %p\n", repeat_string);
+    char *repeat_string = repeat_letter_broken(my_count, 'a');
+    printf("%s location: %p\n", __FUNCTION__, repeat_string);
     // The statement below is likely a segfault as 
     //  the memory from the repeat_letter function
     //  is on the stack and that has been removed when
     //  the function returns.
     //printf("%s\n", repeat_string);
+
+    // We could avoid this by declaring the array here
+    //  and passing a pointer to the array when we call the function
+    char string_array[my_count + 1];
+
+    repeat_letter_working(string_array, my_count, 'a');
+    printf("%s location: %p\n", __FUNCTION__, string_array);
+    printf("%s\n", string_array);
+
     return 0;
 }

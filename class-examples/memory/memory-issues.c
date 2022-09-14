660b3e22624e6988fcf5dc340dabebdcd43435bf
Drew Guarnera <dguarnera@wooster.edu>
2022-09-14 18:52:34 -0400

    Update code examples

---
+++ class-examples/memory/memory-issues.c
@@ -0,0 +1,43 @@
+/* 
+    Simple Example to show memory issues that
+    valgrind can help detect.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* save_string(const char* string) {
+    // strdup will copy a string into a new
+    //  memory buffer on the heap and return
+    //  the pointer to it
+    return strdup(string);
+}
+
+// Create a new string exactly the right size for two strings
+char* concat_string(const char* left, const char* right) {
+    // +1 for null terminator ('\0' or 0)
+    size_t new_string_size = strlen(left) + strlen(right);
+
+    // Like malloc, but fills the memory with 0's
+    char * combined_string = calloc(new_string_size, sizeof(char));
+    
+    // If you use malloc (no memory initialization) the strcpy before the
+    //  strcat is necessary. Strcat relies on there being a null terminator
+    //  to indicate the end of the first string. Strcat will just copy the string
+    //  starting at the beginning of the memory buffer and will keep the null.
+    strcpy(combined_string, left);
+    strcat(combined_string, right);
+
+    return combined_string;
+}
+
+int main(int argc, char *argv[]) {
+    char *my_string = save_string("foo");
+    printf("%s\n", my_string);
+
+    char *combined_string = concat_string(my_string, "bar");
+    printf("%s\n", combined_string);
+
+    return 0;
+}
---
+++ class-examples/memory/resizable_vector.c
@@ -0,0 +1,55 @@
+/* 
+    Simple resizable vector.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct vector {
+    size_t size;
+    int *items;
+};
+
+void push(struct vector* v, int i) {
+    ++(v->size);
+    v->items = realloc(v->items, v->size);
+    assert(v->items != NULL);
+    v->items[v->size - 1] = i;
+}
+
+int pop(struct vector* v) {
+    assert(v->size > 0);
+
+    int value = v->items[v->size - 1];
+    --(v->size);
+    v->items = realloc(v->items, v->size);
+
+    return value;
+}
+
+struct vector init_vector() {
+    struct vector v;
+    v.items = NULL;
+    v.size = 0;
+    return v;
+}
+
+void free_vector(struct vector* v) {
+    free(v->items);
+    v->size = 0;
+}
+
+int main(int argc, char *argv[]) {
+
+    struct vector my_vector = init_vector();
+    push(&my_vector, 10);
+    push(&my_vector, 40);
+    push(&my_vector, 90);
+
+    printf("%d\n", pop(&my_vector));
+
+    free_vector(&my_vector);
+
+    return 0;
+}
